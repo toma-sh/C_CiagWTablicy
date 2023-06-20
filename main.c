661326536a40cbf789b14bb7e7455f9753d09837
toma-sh <zoltom83@gmail.com>
2023-06-20 22:31:27 +0200

    Project template

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>	
+#include <stdlib.h>
+
+/*deklaracja funkcji wypelnijTablice*/
+void wypelnijTablice(int *tab,int rozmiar);
+
+/*deklaracja funkcji znajdzCiag*/
+void znajdzCiag(int *tab,int rozmiar);	
+
+int main(void)/*funkcja glowna*/
+{
+
+	
+    
+	
+
+	return 0;
+}
+/*definicja funkcji wypelniajacej tablice*/
+void wypelnijTablice(int *tab,int rozmiar)
+{
+	
+}
+/*definicja funkcji znajdujacej najdluzszy ciag w tablicy*/
+void znajdzCiag(int *tab,int rozmiar)
+{
+	
+	
+
+
+
+}
